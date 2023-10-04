c3c67b1853d7dec7f36001970cd541719c6dca1b
Hannah Bjorklund <bjorklundhannah@gmail.com>
2023-10-03 21:22:26 -0500

    Completed 3 and 4

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -17,12 +17,14 @@ console.log('Supply changes:', supplyChanges);
 // 3. Create a variable called 'secondItem' and assign it the value of the second
 //    item in the 'supplyChanges' array.
 console.log('3. Access the second value of supplyChanges:');
-
+let secondItem = supplyChanges[1];
+console.log("Second item:", secondItem);
 
 // 4. The last value in the 'supplyChanges' array was added by mistake.
 //    Remove it from the array and store it inside a new variable called 'removedItem'.
 console.log('4. Remove the last value from supplyChanges:');
-
+let removedItem = supplyChanges.pop();
+console.log("Removed item:", removedItem);
 
 // 5. A delivery of 25 more parts arrived. Add the value 25 to the end of the array
 console.log('5. Add the value 25 into supplyChanges.');
