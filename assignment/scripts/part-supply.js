58757ed8596df3be8bc817bae1ac82895b5e88c2
Hannah Bjorklund <bjorklundhannah@gmail.com>
2023-10-04 13:09:30 -0500

    Completed question 6 and added console output of 3 new arrrays

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -28,7 +28,8 @@ console.log("Removed item:", removedItem);
 
 // 5. A delivery of 25 more parts arrived. Add the value 25 to the end of the array
 console.log('5. Add the value 25 into supplyChanges.');
-
+supplyChanges.push(25);
+console.log("Added a new delivery to the end of supply changes", supplyChanges);
 
 // 6. Create three new variables named 'positives', 'negatives', and
 //    'zeroes' whose values are empty arrays. Then, write a for loop
@@ -38,7 +39,22 @@ console.log('5. Add the value 25 into supplyChanges.');
 //      - If the value is a negative number, push it into the 'negatives' array.
 //      - If the value is a zero, push it into the 'zeroes' array.
 console.log('6. Looping through supplyChanges to populate arrays with positive, negative, and zero values:');
+let positives = [];
+let negatives = [];
+let zeroes = [];
 
+for(let i=0; i < supplyChanges.length; i++){
+  if(supplyChanges[i] > 0){
+    positives.push(supplyChanges[i]);
+  } else if(supplyChanges[i] < 0){
+    negatives.push(supplyChanges[i]);
+  } else {
+    zeroes.push(supplyChanges[i]);
+  }
+  console.log('Positives:', positives);
+  console.log('Negatives:', negatives);
+  console.log('Zeroes:', zeroes);
+}
 
 
 
